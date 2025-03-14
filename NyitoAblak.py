5ef16b1d08c1805433183eb701c350c9dd9ea325
Vincentxvx <v.r.rasch@icloud.com>
2025-03-14 13:54:49 +0100

    vegso mára

--- NyitoAblak.py
+++ NyitoAblak.py
@@ -24,16 +24,16 @@ class FilmAblak:
         self.root.minsize(1250, 1000)
 
         
-        self.root.columnconfigure(0, weight=1)
-        self.root.columnconfigure(1, weight=7)
-        self.root.columnconfigure(2, weight=1)
+        self.root.columnconfigure(0, weight=125)
+        self.root.columnconfigure(1, weight=1000)
+        self.root.columnconfigure(2, weight=125)
         self.root.rowconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=6)
         self.root.rowconfigure(2, weight=1)
         self.root.rowconfigure(3, weight=18)
 
        
-        self.kepek = ["./logo.png", "./kep3.jpg", "./kep4.jpg", "./kep5.jpg"]
+        self.kepek = ["./kep3.jpg", "./kep4.jpg", "./kep5.jpg"]
         self.kepekkesz = []
         
         for img_path in self.kepek:
